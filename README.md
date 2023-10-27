c7fd300acee5e90e23baa7e93ebc98620371ad4c
Nneji123 <ifeanyinneji777@gmail.com>
2023-10-27 22:32:46 +0100

    updated readme

---
+++ README.md
@@ -0,0 +1,71 @@
+# Autocommiter Utility Functions
+
+This repository contains utility functions for the Autocommiter tool.
+
+## Introduction
+
+Autocommiter is a Python utility that automates the process of creating a local Git repository, setting up GitHub Actions for scheduled commits, and uploading the repository to GitHub.
+
+## Features
+
+- Create a local Git repository
+- Set up GitHub Actions for scheduled commits
+- Generate a comprehensive README
+- Add a license to your project
+
+## Usage
+
+### Prerequisites
+Before using Autocommiter, make sure you have:
+
+Python installed on your system
+A GitHub account with a personal access token
+
+A Desktop application is also available for Windows users in case you don't have Python installed on your system.
+### Create a Local Repository
+
+1. Run the `create_local_repository` function.
+2. Follow the prompts to provide a folder name and a TXT file name.
+3. Schedule the automatic commits by entering a time in UTC.
+
+### GitHub Actions Configuration
+
+The `create_local_repository` function automatically creates a GitHub Actions workflow file (`.github/workflows/auto_commit.yml`). This workflow is scheduled to run at the specified time every day.
+
+```yaml
+name: Auto Commit
+
+on:
+  schedule:
+    - cron: '[cron_expression]'
+
+permissions:
+  contents: write
+
+jobs:
+  update-text-file:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout Repository
+      uses: actions/checkout@v3
+
+    - name: Update TXT File
+      run: echo $(date) >> [file_name].txt
+
+    - name: Commit Changes
+      run: |
+        git config --local user.email "action@github.com"
+        git config --local user.name "GitHub Action"
+        git add .
+        git commit -am "Auto commit at $(date)"
+        git push
+```
+
+### Author
+[Ifeanyi Nneji](https://github.com/Nneji123)
+
+### Acknowledgments
+Special thanks to the Autocommiter community for their support and contributions.
+
+### License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import sys
+
 from rich import print
 from rich.prompt import Prompt
-from utils import create_local_repository, is_connected_to_internet, upload_to_github
 
-if __name__ == '__main__':
+from utils import create_local_repository, is_connected_to_internet, upload_to_github, is_git_installed
+
+if __name__ == "__main__":
     if is_connected_to_internet() == False:
         print("[red]You are not connected to the internet![/red]")
         sys.exit()
+    if is_git_installed() == False:
+        print("[red]Git is not installed on your system.[/red]")
     else:
-        create_local_repository()
-        upload_repo = Prompt.ask("Do you want to upload the repository to GitHub? (yes/no): ")
 
-        if upload_repo.lower() == 'yes':
+        # Provide a warning and information about obtaining a GitHub access token
+        print("[orange]Before running the application, make sure you have a GitHub access token.[/orange]")
+        print("[orange]You can obtain a GitHub access token by following these steps:[/orange]")
+        print("[orange]1. Visit GitHub Settings: https://github.com/settings/tokens[/orange]")
+        print("[orange]2. Click 'Generate token.'[/orange]")
+        print("[orange]3. Provide a token description and select the necessary scopes (e.g., repo, workflow).[/orange]")
+        print("[orange]4. Click 'Generate token.'[/orange]")
+        print("[orange]5. Copy the generated token and store it in a safe place.[/orange]")
+        print("[orange]For more information visit this site: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens[/orange]")
+
+        create_local_repository()
+
+        upload_repo = Prompt.ask(
+            "Do you want to upload the repository to GitHub? (yes/no): "
+        )
+
+        if upload_repo.lower() == "yes":
             upload_to_github()
         else:
-            print("[yellow]Local repository created. You can manually push it to GitHub later.[/yellow]")
-
+            print(
+                "[orange]Local repository created. You can manually push it to GitHub later.[/orange]"
+            )
